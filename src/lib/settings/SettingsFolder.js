4a223e29b7c496dca161e7d672797ccbd7a12abd
kyranet <kyradiscord@gmail.com>
2019-07-21 01:01:00 +0200

    SettingsFolder: Resolved bug where _patch is called early
    It should be called after successful DB write, but before emitting

--- src/lib/settings/SettingsFolder.js
+++ src/lib/settings/SettingsFolder.js
@@ -355,14 +355,15 @@ class SettingsFolder extends Map {
 		// Patch before emitting the events or synchronizing other shards
 		const updateObject = {};
 		for (const entry of results) mergeObjects(updateObject, makeObject(entry.key, entry.value));
-		this._patch(updateObject);
 
 		if (status === false) {
 			await this.gateway.provider.create(this.gateway.name, this.base.id, results);
 			this.base.existenceStatus = true;
+			this._patch(updateObject);
 			this.base.gateway.client.emit('settingsCreate', this.base);
 		} else {
 			await this.gateway.provider.update(this.gateway.name, this.id, results);
+			this._patch(updateObject);
 			this.base.gateway.client.emit('settingsUpdate', this.base, results.slice());
 		}
 	}
